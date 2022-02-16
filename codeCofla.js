cf3a074b466435265b3a678c0386c6330a3ec7f3
Santiago Salazar <santiago1234ocampo@gmail.com>
2022-02-15 22:50:18 -0300

    Finalizado el codigo en Javascript y los estilos en CSS

--- codeCofla.js
+++ codeCofla.js
@@ -1,23 +1,30 @@
 const container = document.querySelector(".flex-container");
 
+
+
 function crearLlave (nombre, modelo, precio) {
-    img = `<img src="llave.png"`;
+    img = `<img class="llave-img" src="llave.png"`;
     nombre = `<h2>${nombre}</h2>`;
     modelo = `<h3>${modelo}</h3>`;
-    precio = `<p>${precio}</p>`;
+    precio = `<p>Precio: <b>$${precio}</b></p>`;
     return [img, nombre, modelo, precio];
+}
+
+const changeHidden = (number)=> {
+    document.querySelector(".key-data").value = number
 }
 
 let documentFragment = document.createDocumentFragment();
 
 for (var i = 1; i <= 20; i++) {
-    let modeloRandoom = Math.round(Math.random()*10000);
+    let modeloRandom = Math.round(Math.random()*10000);
     let precionRandom = Math.round(Math.random()*10+30);
-    let llave = crearLlave(`Llave: ${i}`, `modelo: #${modeloRandoom}`, `Precio: $${precionRandom}`);
+    let llave = crearLlave(`Llave: ${i}`, `modelo: #${modeloRandom}`, precionRandom);
     let div = document.createElement("div");
-    div.classList.add(`item-${i}`, `flex-item$`);
+    div.addEventListener("click",()=>{changeHidden(modeloRandom)});
+    div.tabIndex = i;
+    div.classList.add(`item-${i}`, `flex-item`);
     div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
-    container.innerHTML += llave;
     documentFragment.appendChild(div);
 }
 
